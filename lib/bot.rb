fe61fa678faddd04a737fc35dde7c24099d48c8f
leolpaz <leonardolpaz95@gmail.com>
2021-04-01 14:28:54 -0300

    Start working on the pizza options method

--- lib/bot.rb
+++ lib/bot.rb
@@ -1,9 +1,11 @@
 require 'telegram/bot'
-require_relative 'pizza.rb'
+require_relative 'pizza'
 
 class Bot
-  def initialize
-    token = '1738560528:AAGEZPyiMmEb9ov6yiumgqLdXXQKoWUjCBQ'
+
+    def initialize
+      token = '1738560528:AAGEZPyiMmEb9ov6yiumgqLdXXQKoWUjCBQ'
+      @@response = Pizza.new
 
     Telegram::Bot::Client.run(token) do |bot|
       bot.listen do |client|
@@ -12,11 +14,20 @@ class Bot
         case client.text
         when '/start'
           initial_text = 'Hello'
-          bot.api.send_message(chat_id: client.chat.id, text: "#{initial_text} #{@@first} #{@@last}")
+          bot.api.send_message(chat_id: client.chat.id, text: "Hello #{@@first} #{@@last}, welcome to my pizza bot, can I take your order? type /pizza to start or /stop to stop")
+        when '/pizza'
+          pizza_option()
         else
-          bot.api.send_message(chat_id: client.chat.id, text: "oops")
+          @@response.api.send_message(chat_id: client.chat.id, text: "oops")
         end
       end
     end
   end
+
+  def pizza_option
+    pizza_text = "Hi, your options are #{@@pizzas[0]} for R$#{@@pizzas['Cheese']} or #{@@pizzas[1]} for R$#{@@pizzas[:Cheese_and_Ham]} or #{@@pizzas[2]} for R$#{@@pizzas[:Meat]} or #{@@pizzas[3]} for R$#{@@pizzas[:Cream_Cheese_and_Chicken]} "
+    bot.api.send_message(chat_id: type.chat.id, text: pizza_text)
+  end
+
+  
 end
