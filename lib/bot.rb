65d24b3d7aaf98b109ec833dbf94d6a002b39ffd
leolpaz <leonardolpaz95@gmail.com>
2021-04-01 17:11:59 -0300

    Finish the pizza hash and the get adress feature

--- lib/bot.rb
+++ lib/bot.rb
@@ -4,13 +4,13 @@ require 'telegram/bot'
 class Bot
 
     def initialize
-      token = '1738560528:AAGEZPyiMmEb9ov6yiumgqLdXXQKoWUjCBQ'
       @state = 0
       @states = {}
-      @pizzas = {1 => 8, 2 => 10, 3 => 15, 4 => 18}
-      @valid_pizza = false
+      @orders = {}
+      @adress = {}
+      @pizzas = {1 => {'name' => 'Cheese Pizza', 'price' => '8'}, 2 => {'name' => 'Cheese and Ham Pizza', 'price' => 10}, 3 => {'name' => 'Meat Pizza', 'price' => 15}, 4 => {'name' => 'Cream Cheese and Chicker Pizza', 'price' => 20}}
 
-    Telegram::Bot::Client.run(token) do |bot|
+    Telegram::Bot::Client.run(@@token) do |bot|
       bot.listen do |client|
         @states[client.chat.id] = 0 unless @states[client.chat.id]
         @@first = client.from.first_name
@@ -25,9 +25,12 @@ class Bot
         else
           if @states[client.chat.id] == 1 and check_pizza(client.text) == false
             pizza_option(bot, client)
-          else
+          elsif @states[client.chat.id] == 1
             @states[client.chat.id] = 2
-            store_order(client.text)
+            store_order(bot, client, client.text, client.chat.id)
+          elsif @states[client.chat.id] == 3
+            get_adress(client.text, client.chat.id)
+            finish_order(bot, client, client.chat.id)
           end
         end
       end
@@ -40,25 +43,46 @@ class Bot
   end
 
   def pizza_option(bot, client)
-    pizza_text = "Hi, your options are: \r\n 1 - Cheese for R$#{@pizzas[1]} \r\n 2 - Cheese and Ham for R$#{@pizzas[2]} \r\n 3 - Meat for R$#{@pizzas[3]} \r\n 4 - Cream Cheese and Chicken for R$#{@pizzas[4]}, \r\n type the number of the pizza that you want to proceed."
+    pizza_text = "Hi, your options are: \r\n 1 - #{@pizzas[1]['name']} for R$#{@pizzas[1]['price']} \r\n 2 - #{@pizzas[2]['name']} for R$#{@pizzas[2]['price']} \r\n 3 - #{@pizzas[3]['name']} for R$#{@pizzas[3]['price']} \r\n 4 - #{@pizzas[4]['name']} for R$#{@pizzas[4]['price']}, \r\n type the number of the pizza that you want to proceed."
     bot.api.send_message(chat_id: client.chat.id, text: pizza_text)
   end
+
+  def store_order(bot, client, order, id)
+    order = order.to_i
+    @orders[id] = order
+    p order 
+    case order
+    when 1
+      bot.api.send_message(chat_id: client.chat.id, text: "You just choose a delicious #{@pizzas[order]['name']} for R$#{@pizzas[order]['price']} please give us your adress")
+      @states[client.chat.id] = 3
+    when 2
+      bot.api.send_message(chat_id: client.chat.id, text: "You just choose a delicious #{@pizzas[order]['name']} for R$#{@pizzas[order]['price']} please give us your adress")
+      @states[client.chat.id] = 3
+    when 3
+      bot.api.send_message(chat_id: client.chat.id, text: "You just choose a delicious #{@pizzas[order]['name']} for R$#{@pizzas[order]['price']} please give us your adress")
+      @states[client.chat.id] = 3
+    when 4
+      bot.api.send_message(chat_id: client.chat.id, text: "You just choose a delicious #{@pizzas[order]['name']} for R$#{@pizzas[order]['price']} please give us your adress")
+      @states[client.chat.id] = 3
+    end
+  end
+
+
+  private
+  @@token = '1738560528:AAGEZPyiMmEb9ov6yiumgqLdXXQKoWUjCBQ'
 
   def check_pizza(pizza)
     pizza = pizza.to_i
     if @pizzas.key?(pizza)
       @pizzas.key?(pizza)
-      @valid_pizza
     else
       false
     end
   end
 
-  def store_order(order)
-    @order = order.to_i
-  case @order
-  when @order == 1
-    bot.api.send_message(chat_id: client.chat.id, text: pizza_text)
+  def get_adress(adress, id)
+    @adress[id] = adress
+    p @adress[id]
   end
-  
+
 end
